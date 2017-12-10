cf8f04c3e929badf6e61f8b112969ff11aa79dc6
Philippe Hamel <p.hamel@protonmail.com>
2017-12-10 16:01:35 -0500

    Quit button

--- interface.py
+++ interface.py
@@ -8,13 +8,16 @@ def word():
 
 window = tk.Tk()
 window.title("Générateur de mots")
-window.configure(bd=20)
+window.configure(bd=10)
 
 label = tk.Label(window, text="", width=30)
 label.grid()
 label.configure(font=('Courrier', 40))
 
 button = tk.Button(window, text="Changer", command=word)
-button.grid()
+button.grid(row=1, column=0, pady=5)
+
+quit = tk.Button(window, text='Quitter', command=window.quit)
+quit.grid(row=1, column=1, pady=5)
 
 window.mainloop()
