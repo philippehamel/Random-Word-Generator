7697715dfb39b8f5d7c4a4ef4840bf4d19220380
Philippe Hamel <p.hamel@protonmail.com>
2017-12-10 16:32:01 -0500

    Twik buttons

--- interface.py
+++ interface.py
@@ -15,9 +15,9 @@ label.grid()
 label.configure(font=('Courrier', 40))
 
 button = tk.Button(window, text="Changer", command=word)
-button.grid(row=1, column=0, pady=5)
+button.grid(row=1)
 
 quit = tk.Button(window, text='Quitter', command=window.quit)
-quit.grid(row=1, column=1, pady=5)
+quit.grid(row=2)
 
 window.mainloop()
